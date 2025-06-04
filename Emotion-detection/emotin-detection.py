36c5fcd957a7aa3a6f3989f45ce265c57d06525f
mmatinm <mmatin.moloudi4@gmail.com>
2025-06-04 18:44:58 +0330

    Update emotin-detection.py

--- Emotion-detection/emotin-detection.py
+++ Emotion-detection/emotin-detection.py
@@ -3,7 +3,6 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
-import os
 import random
 import pandas as pd
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
